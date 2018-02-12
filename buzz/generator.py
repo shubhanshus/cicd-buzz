dc8cf2867179db4fe79207d5b15f650a55e447b3
shubhanshus <33917945+shubhanshus@users.noreply.github.com>
2018-02-11 22:15:00 -0500

    Added one more adverb

--- buzz/generator.py
+++ buzz/generator.py
@@ -4,7 +4,7 @@ buzz = ('continuous testing', 'continuous integration',
     'continuous deployment', 'continuous improvement', 'devops')
 adjectives = ('complete', 'modern', 'self-service', 'integrated', 'end-to-end')
 adverbs = ('remarkably', 'enormously', 'substantially', 'significantly',
-    'seriously')
+    'seriously','terrifically')
 verbs = ('accelerates', 'improves', 'enhances', 'revamps', 'boosts')
 
 def sample(l, n = 1):
